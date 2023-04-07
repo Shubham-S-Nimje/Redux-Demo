6c1db17c28ce80b5a531ec398da12e31236142d9
shubhamnimje77@gmail.com <shubhamnimje77@gmail.com>
2023-04-07 10:47:17 +0530

    first commit

---
+++ redux-demo.js
@@ -0,0 +1,36 @@
+const redux = require('redux');
+
+// const counterReducer = (state = { counter: 0}, action) => {
+//     return {
+//         counter: state.counter + 1
+//     };
+// };
+
+// const store = redux.createStore(counterReducer);
+
+const counterReducer = (state = { counter: 0}, action) => {
+    return {
+        counter: state.counter - 1
+    };
+};
+
+const store2 = redux.createStore(counterReducer);
+
+// console.log(store.getState());
+
+const countersubscriber = () => {
+    // const latestState = store.getState();
+    const latestState = store2.getState();
+    console.log(latestState)
+};
+
+// store.subscribe(countersubscriber);
+store2.subscribe(countersubscriber);
+
+// for(i=0;i<5;i++){
+// store.dispatch({type: 'increment' });
+// }
+
+for(i=0;i<5;i++){
+store2.dispatch({type: 'decrement' });
+}
